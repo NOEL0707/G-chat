0092915027a58f809fbf3031f4f141fea923ca5b
NOEL0707 <70132115+NOEL0707@users.noreply.github.com>
2023-05-16 18:28:00 +0530

    Update FireBaseConfig.jsx

--- GChat-Frontend/src/FireBaseConfig.jsx
+++ GChat-Frontend/src/FireBaseConfig.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ5iGKZWsxw0gEcpsrjm-09B8S9l8_57Q",
-  authDomain: "g-chat-b833e.firebaseapp.com",
-  projectId: "g-chat-b833e",
-  storageBucket: "g-chat-b833e.appspot.com",
-  messagingSenderId: "201759209300",
-  appId: "1:201759209300:web:b949c1f8dd3a69c6a3fac1",
-  measurementId: "G-Z8HK7L3YP3"
+  apiKey: "your_apiKey",
+  authDomain: "your_authDomain",
+  projectId: "your_projectId",
+  storageBucket: "your_storage_bucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId"
 };
 
 // Initialize Firebase
